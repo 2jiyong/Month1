573bd804071aad8e7a211248a5c00dcb5538481a
2jiyong <jdragon2000@naver.com>
2025-02-24 17:30:05 +0900

    함수형 사용해서 리팩토링 완료 여러 함수를 만드는 것도 수월했다.

--- Day16/src/main/java/QRDecoder.java
+++ Day16/src/main/java/QRDecoder.java
@@ -5,95 +5,110 @@ import java.util.Map;
 import java.util.function.Function;
 
 public class QRDecoder {
-    private Map<Integer,String> parseMap = new HashMap<>();
+    private Map<Integer, String> parseMap = new HashMap<>();
 
-    public QRDecoder(){
+    public QRDecoder() {
         initializeMap();
     }
 
-    public void initializeMap(){
-        for(int i = 0 ; i<10;i++){
-            parseMap.put(i,Integer.toString(i));
+    public void initializeMap() {
+        for (int i = 0; i < 10; i++) {
+            parseMap.put(i, Integer.toString(i));
         }
-        for(int i = 10; i<36;i++){
-            parseMap.put(i,Character.toString((char)(i+55)));
+        for (int i = 10; i < 36; i++) {
+            parseMap.put(i, Character.toString((char) (i + 55)));
         }
-        parseMap.put(36," ");
-        parseMap.put(37,"$");
-        parseMap.put(38,"%");
-        parseMap.put(39,"*");
-        parseMap.put(40,"+");
-        parseMap.put(41,"-");
-        parseMap.put(42,".");
-        parseMap.put(43,"/");
-        parseMap.put(44,":");
+        parseMap.put(36, " ");
+        parseMap.put(37, "$");
+        parseMap.put(38, "%");
+        parseMap.put(39, "*");
+        parseMap.put(40, "+");
+        parseMap.put(41, "-");
+        parseMap.put(42, ".");
+        parseMap.put(43, "/");
+        parseMap.put(44, ":");
     }
 
-    private Function<DataBlock,String> createMoveFunction (int startdr,int startdc,int enddr,int enddc, Function<int[][],int[][]> convertFunction){
+    private Function<DataBlock, String> createMoveFunction(int startdr, int startdc, int enddr, int enddc, Function<int[][], int[][]> convertFunction) {
         return dataBlock -> {
-            dataBlock.moveStartCoordinate(startdr,startdc);
-            dataBlock.moveEndCoordinate(enddr,enddc);
+            dataBlock.moveStartCoordinate(startdr, startdc);
+            dataBlock.moveEndCoordinate(enddr, enddc);
             return parseMap.get(converter.applyTransform(convertFunction).apply(dataBlock.readQRArray()));
         };
     }
-    private Function<DataBlock,Integer> createMoveFunctionInt (int startdr,int startdc,int enddr,int enddc, Function<int[][],int[][]> convertFunction){
+
+    private Function<DataBlock, Integer> createMoveFunctionInt(int startdr, int startdc, int enddr, int enddc, Function<int[][], int[][]> convertFunction) {
         return dataBlock -> {
-            dataBlock.moveStartCoordinate(startdr,startdc);
-            dataBlock.moveEndCoordinate(enddr,enddc);
+            dataBlock.moveStartCoordinate(startdr, startdc);
+            dataBlock.moveEndCoordinate(enddr, enddc);
             return converter.applyTransform(convertFunction).apply(dataBlock.readQRArray());
         };
     }
 
     // dataBlock을 받아서 위치를 옮기고, 그 위치의 값을 읽어 String으로 parse
-    public Function<DataBlock,String> upFunction = createMoveFunction(-4,0,-4,0, converter.convertUpToInt);
-    public Function<DataBlock,String> upAfterCWFunction = createMoveFunction(-4,0,-2,-2, converter.convertUpToInt);
-    public Function<DataBlock,String> upSpaceFunction = createMoveFunction(-5,0,-5,0, converter.convertUpToInt);
-    public Function<DataBlock,String> downFunction = createMoveFunction(4,0,4,0, converter.convertDownToInt);
-    public Function<DataBlock,String> downAfterCCWFunction = createMoveFunction(2,0,4,-2, converter.convertDownToInt);
-    public Function<DataBlock,String> downSpaceFunction = createMoveFunction(5,0,5,0, converter.convertDownToInt);
-    public Function<DataBlock,String> CCWFunction = createMoveFunction(-2,-2,-4,0, converter.convertCCWToInt);
-    public Function<DataBlock,String> CWFunction = createMoveFunction(4,-2,2,0, converter.convertCWToInt);
-    public Function<DataBlock,Integer> lengthFunction = createMoveFunctionInt(-4,0,-2,0, converter.convertUpToInt);
+    public Function<DataBlock, String> upFunction = createMoveFunction(-4, 0, -4, 0, converter.convertUpToInt);
+    public Function<DataBlock, String> upAfterCWFunction = createMoveFunction(-4, 0, -2, -2, converter.convertUpToInt);
+    public Function<DataBlock, String> upSpaceFunction = createMoveFunction(-5, 0, -5, 0, converter.convertUpToInt);
+    public Function<DataBlock, String> downFunction = createMoveFunction(4, 0, 4, 0, converter.convertDownToInt);
+    public Function<DataBlock, String> downAfterCCWFunction = createMoveFunction(2, 0, 4, -2, converter.convertDownToInt);
+    public Function<DataBlock, String> downSpaceFunction = createMoveFunction(5, 0, 5, 0, converter.convertDownToInt);
+    public Function<DataBlock, String> CCWFunction = createMoveFunction(-2, -2, -4, 0, converter.convertCCWToInt);
+    public Function<DataBlock, String> CWFunction = createMoveFunction(4, -2, 2, 0, converter.convertCWToInt);
+    public Function<DataBlock, Integer> lengthFunction = createMoveFunctionInt(-4, 0, -2, 0, converter.convertUpToInt);
 
 
-    private List<Function<DataBlock,String>> decodeStep = new ArrayList<>(List.of(upFunction,CCWFunction,downAfterCCWFunction,downFunction,CWFunction,
-            upAfterCWFunction,upFunction,CCWFunction,downAfterCCWFunction,downFunction,CWFunction,upAfterCWFunction,upFunction,upFunction,
-            upSpaceFunction,CCWFunction,downAfterCCWFunction,downSpaceFunction,downFunction,downFunction));
+    private List<Function<DataBlock, String>> decodeStep = new ArrayList<>(List.of(upFunction, CCWFunction, downAfterCCWFunction, downFunction, CWFunction,
+            upAfterCWFunction, upFunction, CCWFunction, downAfterCCWFunction, downFunction, CWFunction, upAfterCWFunction, upFunction, upFunction,
+            upSpaceFunction, CCWFunction, downAfterCCWFunction, downSpaceFunction, downFunction, downFunction));
 
-    private List<Function<DataBlock,String>> errorDecodeStep = new ArrayList<>(List.of());
-
-    public void decode() {
+    public String resultDecode() {
         DataBlock dataBlock = new DataBlock(new int[]{19, 19}, new int[]{20, 20});
         int start = converter.applyTransform(converter.convertStartToInt).apply(dataBlock.readQRArray());
 
-        DataBlock endDataBlock = new DataBlock(new int[]{19,9},new int[]{20,10});
+        DataBlock endDataBlock = new DataBlock(new int[]{19, 9}, new int[]{20, 10});
         int end = converter.applyTransform(converter.convertEndToInt).apply(endDataBlock.readQRArray());
-        if (!(start==12 & end == 6)){
+        if (!(start == 12 & end == 6)) {
             throw new IllegalArgumentException("잘못된 QR입니다.");
         }
         int length = lengthFunction.apply(dataBlock);
 
-        String result = decodeStep.stream().map(step->step.apply(dataBlock))
+        return decodeStep.stream().map(step -> step.apply(dataBlock))
                 .limit(length)
-                .reduce("",(a,b)->a+b);
+                .reduce("", (a, b) -> a + b);
+    }
 
-        DataBlock[] errorDataBlocks = new DataBlock[]{new DataBlock(new int[]{9,7},new int[]{12,8}), new DataBlock(new int[]{9,4},new int[]{12,5}),
-                new DataBlock(new int[]{9,2},new int[]{12,3}),new DataBlock(new int[]{9,0},new int[]{12,1})};
+    private String errorDecode() {
+        DataBlock[] errorDataBlocks = new DataBlock[]{new DataBlock(new int[]{9, 7}, new int[]{12, 8}), new DataBlock(new int[]{9, 4}, new int[]{12, 5}),
+                new DataBlock(new int[]{9, 2}, new int[]{12, 3}), new DataBlock(new int[]{9, 0}, new int[]{12, 1})};
 
         StringBuilder sb = new StringBuilder();
 
         sb.append("0x");
-        for(int i =0; i< errorDataBlocks.length;i++){
-            if (i%2==0){
+        for (int i = 0; i < errorDataBlocks.length; i++) {
+            if (i % 2 == 0) {
                 sb.append(String.format("%2X", converter.applyTransform(converter.convertUpToInt).apply(errorDataBlocks[i].readQRArray())));
-            } else{
+            } else {
                 sb.append(String.format("%2X", converter.applyTransform(converter.convertDownToInt).apply(errorDataBlocks[i].readQRArray())));
             }
         }
+        return sb.toString();
+    }
 
-        String[] QRResult = new String[]{result,sb.toString()};
+    public void decode() {
+        String result = "";
+        String error = "";
+        try {
+            result = resultDecode();
+            error = errorDecode();
+        } catch (IllegalArgumentException e) {
+            System.out.println("잘못된 QR 입니다." + e);
+            result = "";
+            error = "";
+        }
 
-        System.out.println("data = "+QRResult[0]);
-        System.out.println("error = "+QRResult[1]);
+        String[] QRResult = new String[]{result, error};
+
+        System.out.println("data = " + QRResult[0]);
+        System.out.println("error = " + QRResult[1]);
     }
 }
